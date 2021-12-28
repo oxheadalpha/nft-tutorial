dc3fb39506b1f0fea2db7acce8aa277dd8b090f8
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2021-12-28 17:23:39 -0500

    validate TZIP-21 schema

--- packages/fa2-interfaces/src/validate-tzip21.ts
+++ packages/fa2-interfaces/src/validate-tzip21.ts
@@ -1,7 +1,15 @@
 import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
 import schema from './schemas/tzip21-metadata-schema.json';
 import * as v from './validators';
 
 export function validateTzip21(meta: object): string[] {
-  return [];
+  const ajv = new Ajv();
+  addFormats(ajv);
+  ajv.validate(schema, meta);
+  const schemaErrors = ajv.errors
+    ? ajv.errors.map(e => `Error: ${e.instancePath} ${e.message}`)
+    : [];
+
+  return schemaErrors;
 }
