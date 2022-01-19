979c1e5d92bd3161304c9eca3bafdf1d7e0b3e72
Alex Kuprienko <alex.kuprienko@tqtezos.com>
2022-01-18 19:18:04 -0500

    Add comments for transferBatch.

--- packages/fa2-interfaces/src/transfer-batch.ts
+++ packages/fa2-interfaces/src/transfer-batch.ts
@@ -1,6 +1,25 @@
 import { address, nat } from './type-aliases';
 import { Transfer } from './fa2-interface';
 
+/**
+ * A batch builder that can create transfers for the method Fa2Contract.transferTokens
+ * It merges the subsequent transfers with the same source (from_ field)
+ * 
+ * Usage example:
+ * ```typescript
+ * const transfers = transferBatch()
+ *   .withTransfer('tzFromAccount1', 'tzToAccount1', 1, 1)
+ *   .withTransfer('tzFromAccount1', 'tzToAccount2', 2, 1)
+ *   .transfers;
+ *
+ * contract.transferTokens(transfers)
+ * ```
+ *
+ * @param transfers a list of transfers to add more transfers to 
+ * (normally use the default empty list)
+ * 
+ * @returns a batch of transfers that can be populated with transfers
+ */
 export const transferBatch = (transfers: Transfer[] = []) => ({
   transfers,
 
