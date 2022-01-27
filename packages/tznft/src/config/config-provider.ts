4c0627a9f2568306ddc873f0a112176f428344ee
Alex Kuprienko <alex.kuprienko@tqtezos.com>
2022-01-27 17:01:47 -0500

    Make all objects strict.

--- packages/tznft/src/config/config-provider.ts
+++ packages/tznft/src/config/config-provider.ts
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import * as kleur from 'kleur';
 import * as path from 'path';
 
-import { config as configParser, Config } from './parser';
+import { parseConfig, Config } from './parser';
 import { defaultConfig } from './default-config';
 
 const defaultFileName = 'tznft.json';
@@ -16,7 +16,7 @@ export const configProvider = (fileName?: string) => {
 
   const self = {
     filePath,
-    
+
     exists: async (): Promise<boolean> => {
       // fs.exists is deprecated and is not available in fs.promises
       try {
@@ -36,7 +36,7 @@ export const configProvider = (fileName?: string) => {
 
     load: async (): Promise<Config> => {
       const text = await fs.readFile(filePath, { encoding: 'utf8', flag: 'r' });
-      return configParser.parseAsync(JSON.parse(text));
+      return parseConfig(JSON.parse(text));
     },
 
     save: async (config: Config) => {
--- packages/tznft/src/config/parser.ts
+++ packages/tznft/src/config/parser.ts
@@ -1,31 +1,42 @@
-import { unknown, z } from 'zod';
+import { z } from 'zod';
 
 const toPartial = <T>(o: T): Partial<T> => o;
-const partialRecord = <T>(o: z.ZodType<T>) => z.record(o).transform(toPartial)
+const partialRecord = <T>(o: z.ZodType<T>) => z.record(o).transform(toPartial);
 
-const alias = z.object({
-  address: z.string(),
-  secret: z.string().optional()
-});
+const alias = z
+  .object({
+    address: z.string(),
+    secret: z.string().optional()
+  })
+  .strict();
 
-const network = z.object({
-  providerUrl: z.string().url(),
-  lambdaView: z.string().optional(),
-  aliases: partialRecord(alias)
-});
+const network = z
+  .object({
+    providerUrl: z.string().url(),
+    lambdaView: z.string().optional(),
+    aliases: partialRecord(alias)
+  })
+  .strict();
 
-const pinataIpfs = z.object({
-  apiKey: z.string(),
-  secretKey: z.string()
-});
+const pinataIpfs = z
+  .object({
+    apiKey: z.string(),
+    secretKey: z.string()
+  })
+  .strict();
 
-export const config = z.object({
-  activeNetwork: z.string(),
-  availableNetworks: partialRecord(network),
-  pinataIpfs: pinataIpfs.optional()
-});
+const config = z
+  .object({
+    activeNetwork: z.string(),
+    availableNetworks: partialRecord(network),
+    pinataIpfs: pinataIpfs.optional()
+  })
+  .strict();
 
 export type Alias = z.infer<typeof alias>;
 export type Network = z.infer<typeof network>;
 export type PinataIpfs = z.infer<typeof pinataIpfs>;
 export type Config = z.infer<typeof config>;
+
+export const parseConfig = (data: unknown): Promise<Config> =>
+  config.parseAsync(data);
