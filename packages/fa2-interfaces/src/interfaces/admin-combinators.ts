f0895e66f6576a4d9889e19af5e66d913e1ba202
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2022-02-02 18:46:26 -0500

    refactor admin value constructors

--- packages/fa2-interfaces/src/interfaces/admin-combinators.ts
+++ packages/fa2-interfaces/src/interfaces/admin-combinators.ts
@@ -1,15 +1,10 @@
+import { PausableContract } from '..';
 import { address, Contract } from '../type-aliases';
 import {
   MultiAdminContract,
   NonPausableSimpleAdminContract,
   SimpleAdminContract
 } from './admin';
-
-export const SimpleAdmin = (contract: Contract): SimpleAdminContract => ({
-  setAdmin: (new_admin: address) => contract.methods.set_admin(new_admin),
-  confirmAdmin: () => contract.methods.confirm_admin(),
-  pause: (pause: boolean) => contract.methods.pause(pause)
-});
 
 export const NonPausableSimpleAdmin = (
   contract: Contract
@@ -18,9 +13,17 @@ export const NonPausableSimpleAdmin = (
   confirmAdmin: () => contract.methods.confirm_admin()
 });
 
-export const MultiAdmin = (contract: Contract): MultiAdminContract => ({
-  setAdmin: (new_admin: address) => contract.methods.set_admin(new_admin),
-  confirmAdmin: () => contract.methods.confirm_admin(),
-  removeAdmin: (admin: address) => contract.methods.remove_admin(admin),
+const Pausable = (contract: Contract): PausableContract => ({
   pause: (pause: boolean) => contract.methods.pause(pause)
 });
+
+export const SimpleAdmin = (contract: Contract): SimpleAdminContract => {
+  return { ...NonPausableSimpleAdmin(contract), ...Pausable(contract) };
+};
+
+export const MultiAdmin = (contract: Contract): MultiAdminContract => {
+  return {
+    ...SimpleAdmin(contract),
+    removeAdmin: (admin: address) => contract.methods.remove_admin(admin)
+  };
+};
