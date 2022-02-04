211fb7d7db09f591bf11c0f39d3e4760333ce5f1
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2022-02-04 14:56:55 -0500

    token metadata create functions

--- packages/fa2-interfaces/src/index.ts
+++ packages/fa2-interfaces/src/index.ts
@@ -1,6 +1,7 @@
 export * from './fa2-interface';
 export * from './tezos-api';
 export * from './type-aliases';
+export * from './token-metadata';
 export * from './transfer-batch';
 export * from './operator-update-batch';
 export { validateTzip16 } from './validate-tzip16';
---
+++ packages/fa2-interfaces/src/token-metadata.ts
@@ -0,0 +1,91 @@
+import { BigNumber } from 'bignumber.js';
+import { MichelsonMap } from '@taquito/taquito';
+import { char2Bytes } from '@taquito/utils';
+import { TokenMetadataInternal } from './fa2-interface';
+import { nat } from './type-aliases';
+
+/**
+ * Create a contract internal token metadata representation where token metadata
+ * JSON is stored off chain and referred by the `tokenMetadataUri`
+ */
+export const createOffChainTokenMetadata = (
+  tokenId: nat,
+  tokenMetadataUri: string
+): TokenMetadataInternal => {
+  const m: TokenMetadataInternal = {
+    token_id: tokenId,
+    token_info: new MichelsonMap()
+  };
+  m.token_info.set('', char2Bytes(tokenMetadataUri));
+  return m;
+};
+
+/**
+ * Create a contract internal token metadata representation where token metadata
+ * JSON is stored on chain in the contract storage
+ */
+export const createOnChainTokenMetadata = (
+  tokenId: nat,
+  jsonMetadata: string
+): TokenMetadataInternal => {
+  const m: TokenMetadataInternal = {
+    token_id: tokenId,
+    token_info: new MichelsonMap()
+  };
+  m.token_info.set('', char2Bytes('tezos-storage:content'));
+  m.token_info.set('content', char2Bytes(jsonMetadata));
+  return m;
+};
+
+/**
+ * Create a contract internal token metadata representation with on-chain
+ * storage of a few essential attributes.
+ *
+ * @param tokenId token id.
+ * @param name display token name.
+ * @param decimals the position of the decimal point in token balances for display
+ * purposes. The default value is 0. NFT tokens must have decimals value 0.
+ * @param isBooleanAmount describes whether an account can have an amount of
+ * exactly 0 or 1. The default value is false. Should be true for NFT tokens.
+ * @param symbol optional short identifier of the token for display purposes.
+ */
+export const createSimpleTokenMetadata = (
+  tokenId: nat,
+  name: string,
+  decimals?: nat,
+  isBooleanAmount?: boolean,
+  symbol?: string
+) => {
+  const m: TokenMetadataInternal = {
+    token_id: tokenId,
+    token_info: new MichelsonMap()
+  };
+
+  m.token_info.set('name', char2Bytes(name));
+
+  const dec = new BigNumber(decimals || 0);
+  m.token_info.set('decimals', char2Bytes(dec.toString()));
+
+  if (typeof isBooleanAmount !== 'undefined') {
+    if (isBooleanAmount && !dec.eq(0))
+      throw new Error(
+        'Token metadata `isBooleanAmount=true` attribute cannot be used with non-zero `decimals` value.'
+      );
+
+    m.token_info.set('isBooleanAmount', char2Bytes(isBooleanAmount.toString()));
+  }
+
+  if (symbol) m.token_info.set('symbol', char2Bytes(symbol));
+
+  return m;
+};
+
+/**
+ * Create a contract internal non-fungible token metadata representation with
+ * on-chain storage of a few essential attributes.
+ *
+ * @param tokenId token id.
+ * @param name display token name.
+ */
+export const createSimpleNftMetadata = (tokenId: nat, name: string) =>
+  createSimpleTokenMetadata(tokenId, name, 0, true);
