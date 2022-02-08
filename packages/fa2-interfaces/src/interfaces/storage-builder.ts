0bfd56413d8d73ce3f10fccf7cc49d78b851a6b0
Alex Kuprienko <alex.kuprienko@tqtezos.com>
2022-02-07 19:57:20 -0500

    Create POC for storage builders.

---
+++ packages/fa2-interfaces/src/interfaces/storage-builder.ts
@@ -0,0 +1,46 @@
+import { MichelsonMap } from '@taquito/taquito';
+import { address, unit, nat } from '../type-aliases';
+import { TokenMetadataInternal } from './fa2';
+
+export interface StorageBuilder<I, S> {
+  build: (params: I) => S;
+  with: <I1, S1>(sb: StorageBuilder<I1, S1>) => StorageBuilder<I & I1, S & S1>;
+}
+
+export const storageBuilder = <I, S>(
+  buildStorage: (params: I) => S
+): StorageBuilder<I, S> => {
+  const self: StorageBuilder<I, S> = {
+    build: buildStorage,
+    with: <I1, S1>(other: StorageBuilder<I1, S1>) => {
+      const newBuild = (params: I & I1) => {
+        const selfStorage = self.build(params);
+        const otherStorage = other.build(params);
+        return { ...selfStorage, ...otherStorage };
+      };
+      return storageBuilder(newBuild);
+    }
+  };
+
+  return self;
+};
+
+export const SimpleAdmin = storageBuilder(({ owner }: { owner: string }) => ({
+  admin: {
+    admin: owner,
+    pending_admin: undefined,
+    paused: false
+  }
+}));
+
+export const Assets = storageBuilder(() => ({
+  assets: {
+    ledger: new MichelsonMap<nat, address>(),
+    operators: new MichelsonMap<[address, [address, nat]], unit>(),
+    token_metadata: new MichelsonMap<nat, TokenMetadataInternal>(),
+    mint_freeze: false
+  }
+}));
+
+const createStorage = SimpleAdmin.with(Assets).build;
+export const storage = createStorage({ owner: '' });
