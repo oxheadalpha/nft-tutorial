a47ddf9c4c64f0fbe1b33440ef6339b590464f08
Alex Kuprienko <alex.kuprienko@tqtezos.com>
2022-02-09 15:29:25 -0500

    Implement token storage builders.

--- packages/fa2-interfaces/src/interfaces/token-storage.ts
+++ packages/fa2-interfaces/src/interfaces/token-storage.ts
@@ -1,26 +1,49 @@
 import { MichelsonMap } from '@taquito/taquito';
 import { address, unit, nat } from '../type-aliases';
 import { TokenMetadataInternal } from './fa2';
+import { storageBuilder } from './storage-builder';
 
-type OperatorStorage = MichelsonMap<[address, [address, nat]], unit>;
-type TokenMetadataStorage = MichelsonMap<nat, TokenMetadataInternal>;
+const common = storageBuilder(() => ({
+  operators: new MichelsonMap<[address, [address, nat]], unit>(),
+  token_metadata: new MichelsonMap<nat, TokenMetadataInternal>()
+}));
 
-export type NftStorage = {
-  token_metadata: TokenMetadataStorage;
-  ledger: MichelsonMap<nat, address>;
-  operators: OperatorStorage;
-};
+const addAssetsKey = <S>(s: S) => ({ assets: s });
 
-export type FungibleTokenStorage = {
-  ledger: MichelsonMap<address, nat>;
-  operators: OperatorStorage;
-  token_metadata: TokenMetadataStorage;
-  total_supply: nat;
-};
+export const nftStorage = common
+  .withF(() => ({
+    ledger: new MichelsonMap<nat, address>()
+  }))
+  .transform(addAssetsKey);
 
-export type MultiFungibleTokenStorage = {
-  ledger : MichelsonMap<[address, nat], nat>;
-  operators : OperatorStorage;
-  total_supply : MichelsonMap<nat, nat>;
-  token_metadata : TokenMetadataStorage;
-};
+export type NftStorage = ReturnType<typeof nftStorage.build>;
+
+export const fungibleTokenStorage = common
+  .withF(() => ({
+    ledger: new MichelsonMap<address, nat>(),
+    totalSupply: 0
+  }))
+  .transform(addAssetsKey);
+
+export type FungibleTokenStorage = ReturnType<
+  typeof fungibleTokenStorage.build
+>;
+
+export const multiFungibleTokenStorage = common
+  .withF(() => ({
+    ledger: new MichelsonMap<[address, nat], nat>(),
+    totalSupply: 0
+  }))
+  .transform(addAssetsKey);
+
+export type MultiFungibleTokenStorage = ReturnType<
+  typeof multiFungibleTokenStorage.build
+>;
+
+export const mintFreeze = storageBuilder(() => ({
+  mint_freeze: false
+}));
+
+export type MintFreeze = ReturnType<
+  typeof mintFreeze.build
+>;
