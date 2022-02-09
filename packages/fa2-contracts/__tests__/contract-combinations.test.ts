c6c3c869b8e9c5c03ab908906ce5df34c5f6073a
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2022-02-09 17:02:09 -0500

    contract test scaffolding

--- packages/fa2-contracts/__tests__/contract-combinations.test.ts
+++ packages/fa2-contracts/__tests__/contract-combinations.test.ts
@@ -1,6 +1,9 @@
-import {ligo} from '@oxheadalpha/tezos-tools'
+import * as fs from 'fs';
+import * as path from 'path';
+import { ligo } from '@oxheadalpha/tezos-tools';
 import {
   Admin,
+  generateFileContent,
   Implementation,
   Minter,
   MinterAdmin
@@ -35,23 +38,40 @@ const minters: Set<Minter>[] = [
   new Set(['CAN_MINT', 'CAN_BURN', 'CAN_FREEZE'])
 ];
 
-function* combinations(): Generator<[Implementation, Admin, MinterAdmin, Set<Minter>]> {
-  for(let impl of implementations)
-  for(let admin of admins)
-  for(let minterAdmin of minterAdmins)
-  for(let minter of minters)
-    yield [impl, admin, minterAdmin, minter];
+function* combinations(): Generator<
+  [Implementation, Admin, MinterAdmin, Set<Minter>]
+> {
+  for (let impl of implementations)
+    for (let admin of admins)
+      for (let minterAdmin of minterAdmins)
+        for (let minter of minters) {
+          yield [impl, admin, minterAdmin, minter];
+          return;
+        }
 }
 
 describe('test compilation for contract module combinations', () => {
-
-  const ligoEnv = ligo();
+  const testDir = './__tests__/'
+  const ligoEnv = ligo(testDir);
+  const contractFile = path.join(testDir,'fa2_contract.mligo');
 
   test.each([...combinations()])(
+    
     'a combination should compile',
-    (implementation, admin, minterAdmin, minter) => {
-      console.log(implementation, admin, minterAdmin, minter);
-      ligoEnv
+    async (implementation, admin, minterAdmin, minter) => {
+      //console.log(implementation, admin, minterAdmin, minter);
+      const contractCode = generateFileContent({
+        implementation,
+        admin,
+        minterAdmin,
+        minter
+      });
+      fs.writeFileSync(contractFile, contractCode);
+      
+      throw new Error('TEST')
+
+      fs.unlinkSync(contractFile);
     }
   );
+
 });
