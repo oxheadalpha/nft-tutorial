f611e72375ed88a049f49be201faf0357b447d06
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2022-02-15 14:57:35 -0500

    WIP: generator spec command

---
+++ packages/fa2-contracts/src/tzgen-spec.ts
@@ -0,0 +1,9 @@
+export const createGenSpec = (
+  specFile: string,
+  kind: string,
+  admin: string,
+  minter?: string[],
+  minterAdmin?: string
+) => {
+  console.log(specFile, kind, admin, minter, minterAdmin)
+};
--- packages/fa2-contracts/src/tzgen.ts
+++ packages/fa2-contracts/src/tzgen.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Option, program } from 'commander';
 import * as kleur from 'kleur';
 import { TezosOperationError } from '@taquito/taquito';
 import { initConfig } from './tzgen-config';
+import { createGenSpec } from './tzgen-spec';
 const packageJson = require('../package.json');
 
 program.version(packageJson.version);
@@ -12,10 +13,29 @@ program
   .command('init')
   .alias('i')
   .description('create tzgen.json file with the generator environment settings')
-  .option('-l --ligo', 'ligo code directory')
-  .option('-o --compile-out', 'contract compilation output directory')
-  .option('-t --ts', 'TypeScript source directory')
+  .option('-l --ligo', 'ligo code directory. Default is ./ligo')
+  .option('-o --compile-out', 'contract compilation output directory. Default is ./ligo/out')
+  .option('-t --ts', 'TypeScript source directory. Default is ./src')
   .action(async options => initConfig(options.ligo, options.out, options.ts));
+
+//prettier-ignore
+program
+  .command('spec')
+  .alias('s')
+  .description('create specification file for contract generation')
+  .argument('<file>', 'resulting specification file')
+  .addOption(new Option('-k --kind <kind>', 'core FA2 implementation kind')
+    .makeOptionMandatory()
+    .choices(['NFT', 'FT', 'MFT']))
+  .addOption(new Option('-a --admin <admin>', 'type of contract admin')
+    .makeOptionMandatory()
+    .choices(['NO_ADMIN', 'SIMPLE', 'PAUSABLE', 'MULTI']))
+  .addOption(new Option('-m --minter <minter...>', 'optional minter functionality')
+    .choices(['MINT', 'BURN', 'FREEZE']))
+  .addOption(new Option('-ma --minter_admin <minter_admin>', 'minter admin implementation' )
+    .choices(['NO_ADMIN', 'CONTRACT_ADMIN', 'MULTI']))
+  .action((file, options) => createGenSpec(
+    file, options.kind, options.admin, options.minter, options.minter_admin));
 
 program.parseAsync().catch(error => {
   if (typeof error === 'string') console.log(kleur.red(error));
