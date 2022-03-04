29aa11e4549f552c5a268c06e484974adb8e6745
Alex Kuprienko <alex.kuprienko@tqtezos.com>
2022-03-03 19:28:07 -0500

    Add comments.

--- packages/fa2-interfaces/src/interfaces/storage-builder.ts
+++ packages/fa2-interfaces/src/interfaces/storage-builder.ts
@@ -2,12 +2,48 @@ import { MichelsonMap } from '@taquito/taquito';
 import { char2Bytes } from '@taquito/utils';
 import { bytes } from '../type-aliases';
 
+/**
+ * Storage builder is wrapper around function `(params: I) => S`, where
+ * I - input parameters
+ * S - storage that can be used in Taquito.
+ *
+ * StorageBuilder wrapper allows to compose function like the above to form
+ * more complicated functions.
+ */
 export interface StorageBuilder<I, S> {
+  /**
+   * Run the wrapped function and return initial storage
+   */
   build: (params: I) => S;
+
   withF: <I1, S1>(f: (p: I1) => S1) => StorageBuilder<I & I1, S & S1>;
-  with: <I1, S1>(sb: StorageBuilder<I1, S1>) => StorageBuilder<I & I1, S & S1>;
+
+  /**
+   * Compose `this` Storage builder with `other`, returning a new
+   * builder that requires both input parameters for  `this` and
+   * `other` and will return an initial storage that will have
+   * fields of `this` and `other`.
+   */
+  with: <I1, S1>(
+    other: StorageBuilder<I1, S1>
+  ) => StorageBuilder<I & I1, S & S1>;
+
+  /**
+   * @return a new builder that will create storage S1 by applying
+   * function f to storage S
+   */
   transformResult: <S1>(f: (s: S) => S1) => StorageBuilder<I, S1>;
+
+  /**
+   * @return a new builder that will require input parameters I1
+   * by using builder that requires parameters I and applying
+   * function f to the input I1.
+   */
   transformInput: <I1>(f: (p: I1) => I) => StorageBuilder<I1, S>;
+  
+  /**
+   * @return a new builder that does not require any input parameters
+   */
   withParams: (params: I) => StorageBuilder<unknown, S>;
 }
 
@@ -42,6 +78,10 @@ export const storageBuilder = <I, S>(
   return self;
 };
 
+/**
+ * Storage builder that requires just one parameter metadata.
+ * It is usually used as a starting point.
+ */
 export const contractStorage = storageBuilder(
   ({ metadata }: { metadata: string }) => {
     const metaMap = new MichelsonMap<string, bytes>();
