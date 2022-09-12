d717aee21e67fe56b79debd28473c79a8d5b4e13
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2022-09-12 16:56:21 -0400

    fix flextesa image

--- packages/tezos-tools/flextesa/start-sandbox.sh
+++ packages/tezos-tools/flextesa/start-sandbox.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name flextesa-sandbox -e block_time=5  -e flextesa_node_cors_origin="*" --detach -p 20000:20000 oxheadalpha/flextesa:latest kathmandubox start
+docker run --rm --name flextesa-sandbox -e block_time=5  -e flextesa_node_cors_origin="*" --detach -p 20000:20000 oxheadalpha/flextesa:20220715 kathmandubox start
