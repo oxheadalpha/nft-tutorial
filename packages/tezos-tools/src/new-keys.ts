e4fd7b1e3b48ea6934afff3172fd27285738f1dd
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2023-09-20 14:50:19 -0400

    add doc for tezos-tools gen-keys/newKeys

--- packages/tezos-tools/src/new-keys.ts
+++ packages/tezos-tools/src/new-keys.ts
@@ -3,6 +3,11 @@ import { b58cencode, prefix, Prefix } from '@taquito/utils';
 
 import crypto from 'crypto';
 
+/**
+ * @param keyType prefix to append to a generated key. The default value is 'edsk2'
+ * @returns `{pkh, secret}` where `pkh` if a public key hash (tz address) and
+ * `secret` is a secret key.
+ */
 export const newKeys = async (keyType = Prefix.EDSK2) => {
   const keyBytes = Buffer.alloc(32);
   crypto.randomFillSync(keyBytes);
